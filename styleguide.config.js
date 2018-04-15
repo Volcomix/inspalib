207f7098f0dfdfd9fd0e7466c59199ca57290c3f
Volcomix <volcomix@gmail.com>
2018-04-15 16:42:07 +0200

    Set font for all components

---
+++ styleguide.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  components: 'src/components/**/[A-Z]*.js',
+  styles: {
+    StyleGuide: {
+      '@global body': {
+        fontFamily:
+          "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+      },
+    },
+  },
+}
